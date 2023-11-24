440ba8fec9a28fdd3dd4cae30b92cd560ffacf32
DeAngelo-png <148593933+DeAngelo-png@users.noreply.github.com>
2023-11-24 13:50:04 -0800

    Update server.js

--- curl/server.js
+++ curl/server.js
@@ -1,12 +1,11 @@
-const http = require('http');
+const express = require('express');
+const app = express();
+const port = 3000;
 
-const server = http.createServer((req, res) => {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello, World!\n');
+app.get('/', (req, res) => {
+  res.send('Hello, this is your Node.js app responding to curl!\n');
 });
 
-const PORT = process.env.PORT || 3000;
-
-server.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
 });

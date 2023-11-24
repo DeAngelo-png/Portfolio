a5fcd0e6158bdd2f346f56f545317eda3ec12d84
DeAngelo-png <148593933+DeAngelo-png@users.noreply.github.com>
2023-11-24 13:42:04 -0800

    Update server.js

--- curl/server.js
+++ curl/server.js
@@ -1,19 +1,12 @@
-const express = require('express');
-const app = express();
-const port = 3000;
+const http = require('http');
 
-app.get('/', (req, res) => {
-    const userAgent = req.get('User-Agent');
-
-    if (userAgent && userAgent.includes('curl')) {
-        // Respond to curl requests
-        res.send('Hello, curl!');
-    } else {
-        // Respond to regular web browser requests
-        res.send('<h1>Hello, Web Browser!</h1>');
-    }
+const server = http.createServer((req, res) => {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello, World!\n');
 });
 
-app.listen(port, () => {
-    console.log(`Server is running at http://localhost:${port}`);
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });

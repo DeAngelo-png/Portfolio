84acb36424fca81aaf66bbc74c8a5ad2b4c3d95d
DeAngelo-png <148593933+DeAngelo-png@users.noreply.github.com>
2023-11-24 14:13:17 -0800

    Update server.js

--- curl/server.js
+++ curl/server.js
@@ -1,12 +1,18 @@
+// Import the 'http' module
 const http = require('http');
 
+// Create a simple HTTP server that responds with 'Hello, World!' for all requests
 const server = http.createServer((req, res) => {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello, this is your Node.js app responding to curl!\n');
+  // Set the response HTTP header with HTTP status and Content type
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+
+  // Send the response body "Hello, World!"
+  res.end('Hello, World!\n');
 });
 
-const port = 3000;
+// Listen on port 3000 and IP address 127.0.0.1
+const PORT = process.env.PORT || 3000;
 
-server.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`);
+server.listen(PORT, '127.0.0.1', () => {
+  console.log(`Server running at http://127.0.0.1:${PORT}/`);
 });

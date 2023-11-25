c097e378a7605573df4c0be1791f088b25787940
DeAngelo-png <148593933+DeAngelo-png@users.noreply.github.com>
2023-11-25 15:56:32 -0800

    Update index.js

--- curl/index.js
+++ curl/index.js
@@ -1,104 +1,67 @@
-const fs = require('mz/fs');
-const path = require('path');
-const http = require('http');
-const url = require('url');
 const { Readable } = require('stream');
 const colors = require('colors/safe');
 
-// Setup frames in memory
-let original;
-let flipped;
+const asciiArt = `
+                                    ....                                        
+                                .xH888888Hx.                                    
+                               .H8888888888888:                                 
+                               888*"""?""*88888X                                
+                              'f     d8x.   ^%88k                               
+                              '>    <88888X   '?8                               
+                               \`:..:\`888888>    8>                              
+                                      \`"*88     X                               
+                                 .xHHhx.."      !                               
+┌────────────────────────       X88888888hx. ..!       ────────────────────────┐
+│                              !   "*888888888"                                │
+│                                     ^"***"\`                                  │
+│                                                                              │
+│ ---- About Myself ---------------------------------------------------------- │
+│                                                                              │
+│   • Hi, I'm DeAngelo I like to code in my spare time for fun and to explore  │
+│     computer science and languages. I know Python, Bash, a little bit of C,  │
+│     and more listed below here. When I was starting to find interest in      │
+│     Linux, networking, and IoT. I started off with Termux and moved on to an │
+│     actual Linux machine then VPS servers.                                   │
+│                                                                              │
+│ ---- Knowledge ------------------------------------------------------------- │
+│                                                                              │
+│   :: Web Design   8/10 **********                                            │
+│   :: Python       8/10 **********                                            │
+│   :: Wordpress    7/10 **********                                            │
+│   :: Linux        7/10 **********                                            │
+│   :: Bash         7/10 **********                                            │
+│   :: Exploiting   6/10 **********                                            │
+│   :: C++          2/10 ********** ( SLACKING )                               │
+│                                                                              │
+│ ---- What I Do ------------------------------------------------------------- │
+│                                                                              │
+│   • Coding, Exploitation, Web Design                                         │
+│                                                                              │
+│ ---- Portfolio ------------------------------------------------------------- │
+│                                                                              │
+│   • Coming Soon                                                              │
+│                                                                              │
+│                                                                              │
+│                                   Click Me                                   │
+└──────────────────────────────────────────────────────────────────────────────┘
+`;
 
-(async () => {
-  const framesPath = 'frames';
-  const files = await fs.readdir(framesPath);
-
-  original = await Promise.all(files.map(async (file) => {
-    const frame = await fs.readFile(path.join(framesPath, file));
-    return frame.toString();
-  }));
-  flipped = original.map(f => {
-    return f
-      .toString()
-      .split('')
-      .reverse()
-      .join('')
-  })
-})().catch((err) => {
-  console.log('Error loading frames');
-  console.log(err);
-});
-
-const colorsOptions = [
-  'red',
-  'yellow',
-  'green',
-  'blue',
-  'magenta',
-  'cyan',
-  'white'
-];
-const numColors = colorsOptions.length;
-const selectColor = previousColor => {
-  let color;
-
-  do {
-    color = Math.floor(Math.random() * numColors);
-  } while (color === previousColor);
-
-  return color;
-};
-
-const streamer = (stream, opts) => {
-  let index = 0;
-  let lastColor;
+const streamer = (stream) => {
   let frame = null;
-  const frames = opts.flip ? flipped : original;
 
   return setInterval(() => {
     // clear the screen
     stream.push('\033[2J\033[3J\033[H');
 
-    const newColor = lastColor = selectColor(lastColor);
-
-    stream.push(colors[colorsOptions[newColor]](frames[index]));
-
-    index = (index + 1) % frames.length;
+    stream.push(colors.white(asciiArt));
   }, 70);
 };
 
-const validateQuery = ({ flip }) => ({
-  flip: String(flip).toLowerCase() === 'true'
-});
+const server = Readable.from([]);
+const interval = streamer(server);
 
-const server = http.createServer((req, res) => {
-  if (req.url === '/healthcheck') {
-    res.writeHead(200, { 'Content-Type': 'application/json' });
-    return res.end(JSON.stringify({status: 'ok'}));
-  }
+server.pipe(process.stdout);
 
-  if (
-    req.headers &&
-    req.headers['user-agent'] &&
-    !req.headers['user-agent'].includes('curl')
-  ) {
-    res.writeHead(302, { Location: 'https://github.com/hugomd/parrot.live' });
-    return res.end();
-  }
-
-  const stream = new Readable();
-  stream._read = function noop() {};
-  stream.pipe(res);
-  const interval = streamer(stream, validateQuery(url.parse(req.url, true).query));
-
-  req.on('close', () => {
-    stream.destroy();
-    clearInterval(interval);
-  });
-});
-
-const port = process.env.PARROT_PORT || 3000;
-server.listen(port, err => {
-  if (err) throw err;
-  console.log(`Listening on localhost:${port}`);
+process.on('exit', () => {
+  clearInterval(interval);
 });

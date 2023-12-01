4949bfc77c5dcaa947850ebf01c861d10d305f8d
DeAngelo-png <148593933+DeAngelo-png@users.noreply.github.com>
2023-11-30 20:22:48 -0800

    Update server.py

--- curl/server.py
+++ curl/server.py
@@ -5,48 +5,48 @@ from gevent.queue import Queue
 app = Flask(__name__)
 
 ascii_art = '''
-                                    ....                                        
-                                .xH888888Hx.                                    
-                               .H8888888888888:                                 
-                               888*"""?""*88888X                                
-                              'f     d8x.   ^%88k                               
-                              '>    <88888X   '?8                               
-                               `:..:`888888>    8>                              
-                                      `"*88     X                               
-                                 .xHHhx.."      !                               
-┌────────────────────────       X88888888hx. ..!       ────────────────────────┐
-│                              !   "*888888888"                                │
-│                                     ^"***"`                                  │
-│                                                                              │
-│ ---- About Myself ---------------------------------------------------------- │
-│                                                                              │
-│   • Hi, I'm DeAngelo I like to code in my spare time for fun and to explore  │
-│     computer science and languages. I know Python, Bash, a little bit of C,  │
-│     and more listed below here. When I was starting to find interest in      │
-│     Linux, networking, and IoT. I started off with Termux and moved on to an │
-│     actual Linux machine then VPS servers.                                   │
-│                                                                              │
-│ ---- Knowledge ------------------------------------------------------------- │
-│                                                                              │
-│   :: Web Design   8/10 **********                                            │
-│   :: Python       8/10 **********                                            │
-│   :: Wordpress    7/10 **********                                            │
-│   :: Linux        7/10 **********                                            │
-│   :: Bash         7/10 **********                                            │
-│   :: Exploiting   6/10 **********                                            │
-│   :: C++          2/10 ********** ( SLACKING )                               │
-│                                                                              │
-│ ---- What I Do ------------------------------------------------------------- │
-│                                                                              │
-│   • Coding, Exploitation, Web Design                                         │
-│                                                                              │
-│ ---- Portfolio ------------------------------------------------------------- │
-│                                                                              │
-│   • Coming Soon                                                              │
-│                                                                              │
-│                                                                              │
-│                                   Click Me                                   │
-└──────────────────────────────────────────────────────────────────────────────┘
+\x1b[32mroot@hotbockets\x1b[37m:\x1b[34m~\x1b[37m$ \x1b[37m
+                              \x1b{36m      ....                                        
+                              \x1b{36m  .xH888888Hx.                                    
+                              \x1b{36m .H8888888888888:                                 
+                              \x1b{36m 888*"""?""*88888X                                
+                              \x1b{36m'f     d8x.   ^%88k                               
+                              \x1b{36m'>    <88888X   '?8                               
+                              \x1b{36m `:..:`888888>    8>                              
+                              \x1b{36m        `"*88     X                               
+                              \x1b{36m   .xHHhx.."      !                               
+\x1b{37m┌────────────────────────     \x1b{36m  X88888888hx. ..!       \x1b{37m────────────────────────┐
+\x1b{37m│                             \x1b{36m !   "*888888888"                                \x1b{37m│
+\x1b{37m│                             \x1b{36m        ^"***"`                                  \x1b{37m│
+\x1b{37m│                                                                              \x1b{37m│
+\x1b{37m│ ---- About Myself ---------------------------------------------------------- \x1b{37m│
+\x1b{37m│                                                                              \x1b{37m│
+\x1b{37m│   • Hi, I'm DeAngelo I like to code in my spare time for fun and to explore  \x1b{37m│
+\x1b{37m│     computer science and languages. I know Python, Bash, a little bit of C,  \x1b{37m│
+\x1b{37m│     and more listed below here. When I was starting to find interest in      \x1b{37m│
+\x1b{37m│     Linux, networking, and IoT. I started off with Termux and moved on to an \x1b{37m│
+\x1b{37m│     actual Linux machine then VPS servers.                                   \x1b{37m│
+\x1b{37m│                                                                              \x1b{37m│
+\x1b{37m│ ---- Knowledge ------------------------------------------------------------- \x1b{37m│
+\x1b{37m│                                                                              \x1b{37m│
+\x1b{37m│   \x1b{33m:: \x1b{37mWeb Design   8/10 \x1b{33m********\x1b{37m**                                            \x1b{37m│
+\x1b{37m│   \x1b{33m:: \x1b{37mPython       8/10 \x1b{33m********\x1b{37m**                                            \x1b{37m│
+\x1b{37m│   \x1b{33m:: \x1b{37mWordpress    7/10 \x1b{33m*******\x1b{37m***                                            \x1b{37m│
+\x1b{37m│   \x1b{33m:: \x1b{37mLinux        7/10 \x1b{33m*******\x1b{37m***                                            \x1b{37m│
+\x1b{37m│   \x1b{33m:: \x1b{37mBash         7/10 \x1b{33m*******\x1b{37m***                                            \x1b{37m│
+\x1b{37m│   \x1b{33m:: \x1b{37mC++          2/10 \x1b{33m**\x1b{37m********                                            \x1b{37m│
+\x1b{37m│                                                                              \x1b{37m│
+\x1b{37m│ ---- What I Do ------------------------------------------------------------- \x1b{37m│
+\x1b{37m│                                                                              \x1b{37m│
+\x1b{37m│   • Coding, Exploitation, Web Design                                         \x1b{37m│
+\x1b{37m│                                                                              \x1b{37m│
+\x1b{37m│ ---- Portfolio ------------------------------------------------------------- \x1b{37m│
+\x1b{37m│                                                                              \x1b{37m│
+\x1b{37m│   • Coming Soon                                                              \x1b{37m│
+\x1b{37m│                                                                              \x1b{37m│
+\x1b{37m│                                                                              \x1b{37m│
+\x1b{37m│                                                                              \x1b{37m│
+\x1b{37m└──────────────────────────────────────────────────────────────────────────────┘
 '''
 
 stream_queue = Queue()
